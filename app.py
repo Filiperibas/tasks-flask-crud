66fe0e8356e74debb1f9bec954eae24736b2ec77
Filiperibas <fellps77.plo@gmail.com>
2025-05-20 17:36:00 -0400

    Criação do Parâmetro GET

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from models.task import Task
 
 # __name__ = "main"
@@ -10,12 +10,36 @@ app = Flask(__name__)
 # Tabela: Tarefa
 
 tasks = []
+task_id_control = 1
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
+    global task_id_control
     data = request.get_json()
-    print(data)
-    return 'Test'
+    new_task = Task(id=task_id_control, title=data.get("title"), description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+
+    output = {
+            "tasks": task_list,
+            "total_tasks": len(task_list)
+        }
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+
+    return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
